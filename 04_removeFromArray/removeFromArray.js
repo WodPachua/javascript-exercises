7f90c96cbd03d3991bfea2198ac8422f68d5dd6f
wodpachua <molarag21996@gmail.com>
2024-03-02 23:00:13 +0300

    Fix exercise-4 tests to all pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...toRemove) {
+    let newArray = [];
+    
+    oldArray.forEach(arrayItem => {
+        if (!toRemove.includes(arrayItem)){
+            newArray.push(arrayItem);
+        }
+    })
+    
 
+    return newArray;
 };
 
 // Do not edit below this line
