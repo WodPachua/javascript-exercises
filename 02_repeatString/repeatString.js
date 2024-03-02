55d0bb5d4e52b5099d0dfd14a528896925181ee8
wodpachua <molarag21996@gmail.com>
2024-03-02 19:56:59 +0300

    Fix all 7 tests to pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let outPut = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      outPut += string;
+    }
+  } else {
+    outPut = "ERROR";
+  }
 
+  return outPut;
 };
 
 // Do not edit below this line
