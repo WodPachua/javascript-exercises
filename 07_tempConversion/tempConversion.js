31edecdc619aabd6df45da1b33824598c6589c6d
wodpachua <molarag21996@gmail.com>
2024-03-03 00:36:41 +0300

    Fix exercise-7 for all tests to pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return roundToOne((fahrenheitTemp - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return roundToOne((celsiusTemp * (9 / 5) + 32));
 };
 
+function roundToOne(num) {
+  return Math.round(num * 10) / 10;
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
