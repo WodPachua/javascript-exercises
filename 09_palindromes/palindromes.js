8eb1379fbbfe13f6bc9755e1f64d65f934300f77
wodpachua <molarag21996@gmail.com>
2024-03-08 15:38:53 +0300

    Fix Palindromes function to pass all texts

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    const plainInput = input.replace(/[\W]|_/g, '').toLowerCase();
+    return plainInput === plainInput.split('').reverse().join('').toLowerCase();
 };
 
 // Do not edit below this line
