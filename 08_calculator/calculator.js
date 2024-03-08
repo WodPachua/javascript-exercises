625cc9ad918c4032798628e572dcf411d288f079
wodpachua <molarag21996@gmail.com>
2024-03-08 14:23:12 +0300

    Fix calculator functions for all tests to pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else { 
+    // num * factorial(num -1)
+    let result = 1;
+    for (num; num > 0; num--) {
+      result *= num;
+    }
+
+    return result;
+  };
 };
 
 // Do not edit below this line
