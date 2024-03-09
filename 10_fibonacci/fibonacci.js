804f474021bc47a48b8a403e45a246d3fc6240ff
wodpachua <molarag21996@gmail.com>
2024-03-09 21:13:42 +0300

    Fix Fibonancci Series function so that tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(numIndex) {
+    numIndex = Number.parseInt(numIndex);
+    let previous = 0;
+    let current = 1;
+    let next;
+if (numIndex > 0) {
+    for (let index = 1; index < numIndex; index++) {
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
 
+    return current;
+
+} else if (numIndex === 0) {
+    return 0;
+} else {
+    return 'OOPS'
+};
 };
 
 // Do not edit below this line
