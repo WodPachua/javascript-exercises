11958d1626bd557249c2f2421fa98a35120667ea
wodpachua <molarag21996@gmail.com>
2024-03-10 00:19:35 +0300

    Fix findTheOldest function to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    return arr.reduce((older, person) => {
+        const olderAge = currentAge(older.yearOfBirth, older.yearOfDeath);
+        const personAge = currentAge(
+          person.yearOfBirth,
+          person.yearOfDeath
+        );
+        return olderAge < personAge ? person : older;
+      });
+    };
+    
+    const currentAge = function (yearBirth, yearDeath) {
+      if (!yearDeath) {
+        yearDeath = new Date().getFullYear();
+      }
+      return yearDeath - yearBirth;
 
 };
 
